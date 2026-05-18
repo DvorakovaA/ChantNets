aa7d2a0dfd7281cea72b8c604141928e52002f6f
ChantMapper <chantmapper@gmail.com>
2026-05-18 21:07:14 +0200

    feat: basic SB model implementation

--- experiments/sbmodel.py
+++ experiments/sbmodel.py
@@ -3,26 +3,50 @@
 """
 import pickle
 import graph_tool.all as gt
-
+from collections import defaultdict
 
 class SBModel:
     """
     A wrapper for graph-tool's SBM fitting functions that allows us to easily
     fit an SBM to a graph and keep the best result.
     """
-    def __init__(self, graph):
-        self.graph = graph
+    def __init__(self):
+        self.graph = None
         self.model = None
         self.n_init = 10
+        self.deg_corrected = True
+        self.states = defaultdict(list)
 
+    def load_graph(self, path):
+        """
+        Load a graph from a file.
+        """
+        self.graph = gt.load_graph(path)
+        print(f"Loaded graph with {self.graph.num_vertices()} vertices, {self.graph.num_edges()} edges")
 
+    def get_states(self):
+        """
+        Return states from all fitting attempts.
+        """
+        return self.states
+    
     def fit_sbm(self, n_init=10):
         """
         Try fitting SBM to the graph using graph-tool's built-in functions
         multiple times and keep the best result.
         """
-        # gt.minimize_blockmodel_dl
-        pass
+        self.states = defaultdict(list)
+        print(f"Fitting SBM to graph with {self.graph.num_vertices()} vertices and {self.graph.num_edges()} edges...")
+        for i in range(n_init):
+            print(f"Fitting SBM (init {i+1}/{n_init})...")
+            args = dict(deg_corr=self.deg_corrected, clabel=self.graph.vp["type"])
+            state = gt.minimize_blockmodel_dl(self.graph, state_args=args)
+            
+            self.states['DC_SBM'].append({"state": state, "model": state.entropy()})
+            print(f"[{i+1}/{n_init}] entropy = {state.entropy():.2f}")
+
+        best = min(self.states['DC_SBM'], key=lambda s: s["model"])
+        self.model = best["model"]
 
 
     def fit_nested_sbm(self, n_init=10):
@@ -30,8 +54,18 @@ class SBModel:
         Try fitting nested SBM to the graph using graph-tool's built-in functions
         multiple times and keep the best result.
         """
-        # gt.minimize_nested_blockmodel_dl
-        pass
+        self.states = defaultdict(list)
+        print(f"Fitting nested SBM to graph with {self.graph.num_vertices()} vertices and {self.graph.num_edges()} edges...")
+        for i in range(n_init):
+            print(f"Fitting nested SBM (init {i+1}/{n_init})...")
+            state = gt.minimize_nested_blockmodel_dl(self.graph,
+                        state_args=dict(deg_corr=self.deg_corrected, clabel=self.graph.vp["type"]))
+            self.states['Nested_DC_SBM'].append({"state": state, "model": state.entropy()})
+            print(f"[{i+1}/{n_init}] entropy = {state.entropy():.2f}")
+
+        best = min(self.states['Nested_DC_SBM'], key=lambda s: s["model"])
+        self.model = best["model"]
+        print(self.states)
 
     def fit_sbm_weighted(self, weight_label, n_init=10):
         """
@@ -50,14 +84,15 @@ class SBModel:
         # gt.minimize_nested_blockmodel_dl
         pass
 
-    def load_model(self, model):
+
+    def load_states(self, path):
         """
         Load a previously fitted model.
         """
-        self.model = model
+        self.states = pickle.load(open(path, 'rb'))
     
-    def save_model(self, path):
+    def save_states(self, path):
         """
         Save the fitted model to a file.
         """
-        pass
+        pickle.dump(self.states, open(path, 'wb'))
--- experiments/utils.py
+++ experiments/utils.py
@@ -22,13 +22,13 @@ def construct_bipart_source_feast_graph(corpus):
             source_vertex = g.add_vertex()
             source_map[chant.srclink] = source_vertex
             vprop_name[source_vertex] = chant.srclink
-            vprop_type[source_vertex] = 'source'
+            vprop_type[source_vertex] = 0 #'source'
 
         if chant.feast not in feast_map:
             feast_vertex = g.add_vertex()
             feast_map[chant.feast] = feast_vertex
             vprop_name[feast_vertex] = chant.feast
-            vprop_type[feast_vertex] = 'feast'
+            vprop_type[feast_vertex] = 1 #'feast'
 
         g.add_edge(source_map[chant.srclink], feast_map[chant.feast])
 
